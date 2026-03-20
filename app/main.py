809ac5c7c0e7e59ec94196142f8a93f194ae4aac
davlatbekkhasanov-spec <davlatbek.khasanov@icloud.com>
2026-03-20 20:11:50 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 import asyncio
+import logging
+
 from app.telegram_bot import dp, bot
 
 async def main():
+    logging.basicConfig(level=logging.INFO)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
